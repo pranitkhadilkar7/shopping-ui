609726f95111592a8693a71185bd9d8a6bd5cf4e
Pranit Khadilkar <pranit@emtech.com>
2024-03-06 14:22:26 +0530

    added react with typescript using webpack

---
+++ webpack.config.ts
@@ -0,0 +1,38 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import path from 'path'
+import { Configuration } from 'webpack'
+
+const config: Configuration = {
+    mode: 'production',
+    entry: './src/index.tsx',
+    devtool: 'inline-source-map',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ["ts-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
+
+export default config
